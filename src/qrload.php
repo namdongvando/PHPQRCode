6cc521c2b2e82af58a43eafabd226a780e659a24
admin <namdong92@gmail.com>
2025-12-02 15:02:05 +0700

    Add Class Load file

---
+++ src/qrload.php
@@ -0,0 +1,40 @@
+<?php
+namespace PHPQRCODE;
+class qrload
+{
+    public function __construct()
+    {
+        $QR_BASEDIR = dirname(__FILE__) . DIRECTORY_SEPARATOR;
+        include $QR_BASEDIR . "qrconst.php";
+        include $QR_BASEDIR . "qrconfig.php";
+        include $QR_BASEDIR . "qrtools.php";
+        include $QR_BASEDIR . "qrspec.php";
+        include $QR_BASEDIR . "qrimage.php";
+        include $QR_BASEDIR . "qrinput.php";
+        include $QR_BASEDIR . "qrbitstream.php";
+        include $QR_BASEDIR . "qrsplit.php";
+        include $QR_BASEDIR . "qrrscode.php";
+        include $QR_BASEDIR . "qrmask.php";
+        include $QR_BASEDIR . "qrencode.php";
+
+    }
+    static public function LoadLib()
+    {
+        $QR_BASEDIR = dirname(__FILE__) . DIRECTORY_SEPARATOR;
+        include $QR_BASEDIR . "qrconst.php";
+        include $QR_BASEDIR . "qrconfig.php";
+        include $QR_BASEDIR . "qrtools.php";
+        include $QR_BASEDIR . "qrspec.php";
+        include $QR_BASEDIR . "qrimage.php";
+        include $QR_BASEDIR . "qrinput.php";
+        include $QR_BASEDIR . "qrbitstream.php";
+        include $QR_BASEDIR . "qrsplit.php";
+        include $QR_BASEDIR . "qrrscode.php";
+        include $QR_BASEDIR . "qrmask.php";
+        include $QR_BASEDIR . "qrencode.php";
+    }
+
+}
+
+
+
